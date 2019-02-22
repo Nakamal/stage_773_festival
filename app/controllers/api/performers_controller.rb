5905e13fa900ffcb7492e3177dd0bea52f2a5db4
Nakamal <nickhaugland@gmail.com>
2019-02-22 15:03:59 -0600

    performer logic added for index, show, update and destroy

--- app/controllers/api/performers_controller.rb
+++ app/controllers/api/performers_controller.rb
@@ -1,5 +1,7 @@
 class Api::PerformersController < ApplicationController
   def index
+    @performers = Performer.all  
+
     render 'index.json.jbuilder'
   end
 
@@ -8,14 +10,20 @@ class Api::PerformersController < ApplicationController
   end
 
   def show
+    @performer = Performer.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def update
+    @performer = Performer.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def destroy
+    @performer = Performer.find(params[:id])
+    
     render json: {message: "Successfully Destroyed!"}
   end
 end
