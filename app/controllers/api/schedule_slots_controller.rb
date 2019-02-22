f07499488b2ad28bde0c5ed8398327ec7094a71a
Nakamal <nickhaugland@gmail.com>
2019-02-22 15:06:33 -0600

    schedule_slot logic added for index, show, update and destroy

--- app/controllers/api/schedule_slots_controller.rb
+++ app/controllers/api/schedule_slots_controller.rb
@@ -1,5 +1,7 @@
 class Api::ScheduleSlotsController < ApplicationController
   def index
+    @schedule_slots = ScheduleSlot.all
+
     render 'index.json.jbuilder'
   end
 
@@ -8,14 +10,20 @@ class Api::ScheduleSlotsController < ApplicationController
   end
 
   def show
+    @schedule_slot = ScheduleSlot.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def update
+    @schedule_slot = ScheduleSlot.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def destroy
+    @schedule_slot = ScheduleSlot.find(params[:id])
+    
     render json: {message: "Successfully Destroyed!"}
   end
 end
