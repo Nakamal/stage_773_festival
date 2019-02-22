04dac3ffa664b7726e49c5a9a00b6ef5e02f9a61
Nakamal <nickhaugland@gmail.com>
2019-02-22 14:46:37 -0600

    person logic added for index, show, update and destroy

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,5 +1,7 @@
 class Api::PeopleController < ApplicationController
   def index
+    @people = Person.all
+
     render 'index.json.jbuilder'
   end
 
@@ -8,14 +10,20 @@ class Api::PeopleController < ApplicationController
   end
 
   def show
+    @person = Person.find(params[:id])
+    
     render 'show.json.jbuilder'
   end
 
   def update
+    @person = Person.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def destroy
+    @person = Person.find(params[:id])
+
     render json: {message: "Successfully Destroyed!"}
   end
 end
