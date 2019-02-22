ed126d5c4ba328b4afd37ded563a61e5b5d6576b
Nakamal <nickhaugland@gmail.com>
2019-02-22 14:42:03 -0600

    festival _application  logic added for index, show, update and destroy

--- app/controllers/api/festival_applications_controller.rb
+++ app/controllers/api/festival_applications_controller.rb
@@ -1,5 +1,7 @@
 class Api::FestivalApplicationsController < ApplicationController
   def index
+    @festival_applications = FestivalApplication.all
+
     render 'index.json.jbuilder'
   end
 
@@ -8,14 +10,20 @@ class Api::FestivalApplicationsController < ApplicationController
   end
 
   def show
+    @festival_application = FestivalApplication.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def update
+    @festival_application = FestivalApplication.find(params[:id])
+
     render 'show.json.jbuilder'
   end
 
   def destroy
+    @festival_application = FestivalApplication.find(params[:id])
+    
     render json: {message: "Successfully Destroyed!"}
   end
 end
