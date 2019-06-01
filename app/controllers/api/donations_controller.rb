f57dade1689cd8514afafa8d89afc6e253879bea
Nakamal <nickhaugland@gmail.com>
2019-06-01 12:27:04 -0500

    added update, create, show and destroy specifics for donation

--- app/controllers/api/donations_controller.rb
+++ app/controllers/api/donations_controller.rb
@@ -6,24 +6,39 @@ class Api::DonationsController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    @donation = Donation.new(
+                          sponser_id: params[:sponser_id],
+                          amount: params[:amount],
+                          start_date: params[:start_date]
+                          )
+    if @donation.save 
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @donation.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
     @donation = Donation.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @donation = Donation.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @donation.sponser_id = params[:sponser_id] || @donation.sponser_id
+    @donation.amount = params[:amount] || @donation.amount
+    @donation.start_date = params[:start_date] || @donation.start_date
+
+    if @donation.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @donation.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @donation = Donation.find(params[:id])
-
-    render json: {message: "Successfully Destroyed!"}
+    render json: {message: "Donation Removed"}
   end
 end
