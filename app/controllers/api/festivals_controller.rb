92bfa40aa355e9d842a048f0199643d72bc81db7
Nakamal <nickhaugland@gmail.com>
2019-06-01 13:09:09 -0500

    added update, create, show and destroy specifics for festival

--- app/controllers/api/festivals_controller.rb
+++ app/controllers/api/festivals_controller.rb
@@ -6,7 +6,23 @@ class Api::FestivalsController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    if current_user && current_user.admin
+      
+      @festival = Festival.new(
+                            application_start: params[:application_start],
+                            application_end: params[:application_end],
+                            public_start: params[:public_start],
+                            name: params[:name],
+                            description: params[:description]
+                            )
+      if @festival.save 
+        render 'show.json.jbuilder'
+      else
+        render json: { errors: @festival.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized  
+    end
   end
 
   def show
@@ -17,12 +33,22 @@ class Api::FestivalsController < ApplicationController
   def update
     @festival = Festival.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @festival.application_start = params[:application_start] || @festival.application_start
+    @festival.application_end = params[:application_end] || @festival.application_end
+    @festival.public_start = params[:public_start] || @festival.public_start
+    @festival.name = params[:name] || @festival.name
+    @festival.description = params[:description] || @festival.description
+
+    if @festival.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @festival.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @festival = Festival.find(params[:id])
-    
-    render json: {message: "Successfully Destroyed!"}
+    @festival.destroy
+    render json: {message: "Festival Removed!"}
   end
 end
