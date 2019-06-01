dcfc884ce5a68145acd648b20cc5d03ccc2256f7
Nakamal <nickhaugland@gmail.com>
2019-06-01 12:17:46 -0500

    added update, create, show and destroy specifics for event

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -6,24 +6,51 @@ class Api::EventsController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    if current_user && current_user.admin
+      
+      @event = Event.new(
+                            show: params[:show],
+                            name: params[:name],
+                            description: params[:description],
+                            rating: params[:rating],
+                            video_link: params[:video_link],
+                            festival_id: params[:festival_id]
+                            )
+      if @event.save 
+        render 'show.json.jbuilder'
+      else
+        render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized  
+    end
   end
 
   def show
     @event = Event.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @event = Event.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @event.show = params[:show] || @event.show
+    @event.name = params[:name] || @event.name
+    @event.description = params[:description] || @event.description
+    @event.rating = params[:rating] || @event.rating
+    @event.video_link = params[:video_link] || @event.video_link
+    @event.festival_id = params[:festival_id] || @event.festival_id
+
+    if @event.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @event = Event.find(params[:id])
-    
-    render json: {message: "Successfully Destroyed!"}
+    @event.destroy
+    render json: {message: "Event Deleted"}
   end
 end
