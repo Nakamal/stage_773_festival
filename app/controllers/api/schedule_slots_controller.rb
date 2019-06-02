4230ae9694c464570025e0cdf0656f70e0c6064e
Nakamal <nickhaugland@gmail.com>
2019-06-02 14:41:03 -0500

    added update, create, show and destroy specifics for schedule slot

--- app/controllers/api/schedule_slots_controller.rb
+++ app/controllers/api/schedule_slots_controller.rb
@@ -6,24 +6,51 @@ class Api::ScheduleSlotsController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    if current_user && current_user.admin
+      
+      @schedule_slot = ScheduleSlot.new(
+                            main_eventable_id: params[:main_eventable_id],
+                            main_eventable_type: params[:main_eventable_type],
+                            start_time: params[:start_time],
+                            end_time: params[:end_time],
+                            venue: params[:venue],
+                            other_location: params[:other_location]
+                            )
+      if @schedule_slot.save 
+        render 'show.json.jbuilder'   
+      else
+        render json: { errors: @schedule_slot.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized  
+    end
   end
 
   def show
     @schedule_slot = ScheduleSlot.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @schedule_slot = ScheduleSlot.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @schedule_slot.main_eventable_id = params[:main_eventable_id] || @schedule_slot.main_eventable_id
+    @schedule_slot.main_eventable_type = params[:main_eventable_type] || @schedule_slot.main_eventable_type
+    @schedule_slot.start_time = params[:start_time] || @schedule_slot.start_time
+    @schedule_slot.end_time = params[:end_time] || @schedule_slot.end_time
+    @schedule_slot.venue = params[:venue] || @schedule_slot.venue
+    @schedule_slot.other_location = params[:other_location] || @schedule_slot.other_location
+
+    if @schedule_slot.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @schedule_slot.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @schedule_slot = ScheduleSlot.find(params[:id])
-    
-    render json: {message: "Successfully Destroyed!"}
+    @schedule_slot.destroy
+    render json: {message: "Slot Removed"}
   end
 end
