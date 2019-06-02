3d7186099df951d7509324a74e42e117de6ee2ce
Nakamal <nickhaugland@gmail.com>
2019-06-02 14:27:46 -0500

    added update, create, show and destroy specifics for performer

--- app/controllers/api/performers_controller.rb
+++ app/controllers/api/performers_controller.rb
@@ -6,24 +6,47 @@ class Api::PerformersController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    if current_user && current_user.admin
+      
+      @performer = Performer.new(
+                            festival_application_id: params[:festival_application_id],
+                            name: params[:name],
+                            role: params[:role]
+                            )
+      if @performer.save 
+        render 'show.json.jbuilder'
+      else
+        render json: { errors: @performer.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized  
+    end
   end
 
   def show
     @performer = Performer.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @performer = Performer.find(params[:id])
 
+    @performer.festival_application_id = params[:festival_application_id] || @performer.festival_application_id
+    @performer.name = params[:name] || @performer.name
+    @performer.role = params[:role] || @performer.role
+
+    if @performer.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @performer.errors.full_messages }, status: :unprocessable_entity
+    end
+
     render 'show.json.jbuilder'
   end
 
   def destroy
     @performer = Performer.find(params[:id])
-    
-    render json: {message: "Successfully Destroyed!"}
+    @performer.destroy
+    render json: {message: "Performer Removed"}
   end
 end
