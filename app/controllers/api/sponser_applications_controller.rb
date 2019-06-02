5b5d370d36aa497c00c85046ab5d3dc05083b247
Nakamal <nickhaugland@gmail.com>
2019-06-02 14:47:07 -0500

    added update, create, show and destroy specifics for sponser application

--- app/controllers/api/sponser_applications_controller.rb
+++ app/controllers/api/sponser_applications_controller.rb
@@ -6,24 +6,46 @@ class Api::SponserApplicationsController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    @sponser_application = SponserApplication.new(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          company: params[:company],
+                          email: params[:email],
+                          subject: params[:subject],
+                          body: params[:body]
+                          )
+    if @sponser_application.save 
+      render 'show.json.jbuilder'  
+    else
+      render json: { errors: @sponser_application.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
     @sponser_application = SponserApplication.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @sponser_application = SponserApplication.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @sponser_application.first_name = params[:first_name] || @sponser_application.first_name
+    @sponser_application.last_name = params[:last_name] || @sponser_application.last_name
+    @sponser_application.company = params[:company] || @sponser_application.company
+    @sponser_application.email = params[:email] || @sponser_application.email
+    @sponser_application.subject = params[:subject] || @sponser_application.subject
+    @sponser_application.body = params[:body] || @sponser_application.body
+
+    if @sponser_application.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @sponser_application.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @sponser_application = SponserApplication.find(params[:id])
-    
-    render json: {message: "Successfully Destroyed!"}
+    @sponser_application.destroy
+    render json: {message: "Sponser Removed"}
   end
 end
