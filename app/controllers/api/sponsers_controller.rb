e37cc6d698329b9e2e49428c74f0c6c2696463d4
Nakamal <nickhaugland@gmail.com>
2019-06-02 14:53:13 -0500

    added update, create, show and destroy specifics for sponsers

--- app/controllers/api/sponsers_controller.rb
+++ app/controllers/api/sponsers_controller.rb
@@ -6,24 +6,47 @@ class Api::SponsersController < ApplicationController
   end
 
   def create
-    render 'show.json.jbuilder'
+    if current_user && current_user.admin
+      
+      @sponser = Sponser.new(
+                            name: params[:name],
+                            website_link: params[:website_link],
+                            video_link: params[:video_link],
+                            foundation: params[:foundation]
+                            )
+      if @sponser.save 
+        render 'show.json.jbuilder'
+      else
+        render json: { errors: @sponser.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: {}, status: :unauthorized  
+    end
   end
 
   def show
     @sponser = Sponser.find(params[:id])
-
     render 'show.json.jbuilder'
   end
 
   def update
     @sponser = Sponser.find(params[:id])
 
-    render 'show.json.jbuilder'
+    @product.name = params[:name] || @product.name
+    @product.website_link = params[:website_link] || @product.website_link
+    @product.video_link = params[:video_link] || @product.video_link
+    @product.foundation = params[:foundation] || @product.foundation
+
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @sponser = Sponser.find(params[:id])
-
-    render json: {message: "Successfully Destroyed!"}
+    @sponser.destroy
+    render json: {message: "Sponser Removed"}
   end
 end
